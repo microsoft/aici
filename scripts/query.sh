8388db29d3a57e51dd23eaaf23a9a68bdadc42df
Michal Moskal <michal@moskal.me>
2023-10-12 23:56:25 +0000

    stream response

--- scripts/query.sh
+++ scripts/query.sh
@@ -5,5 +5,6 @@ curl http://127.0.0.1:8080/v1/completions \
     "model": "",
     "prompt": "Say this is a test",
     "max_tokens": 7,
-    "temperature": 0
+    "temperature": 0,
+    "stream": true
   }'
