3aaea61079fa1bd8e7fc13612242bd2418e87b96
Michal Moskal <michal@moskal.me>
2023-10-12 00:40:28 +0000

    advancing steps

--- aici_ast_runner/src/ast_runner.rs
+++ aici_ast_runner/src/ast_runner.rs
@@ -3,7 +3,7 @@ mod rx;
 use serde::{Deserialize, Serialize};
 use std::rc::Rc;
 
-use crate::rx::{RecRx, RecRxState};
+use crate::rx::RecRx;
 
 use aici_abi::{
     aici_expose_all,
@@ -34,6 +34,7 @@ pub struct Runner {
     curr_step: usize,
     trie: Rc<Box<TokTrie>>,
     rec: Box<dyn Recognizer>,
+    tok_limit: usize,
 }
 
 #[derive(Clone)]
@@ -46,7 +47,7 @@ impl FunctionalRecognizer<u32> for FixedArgs {
         0
     }
 
-    fn append(&self, state: u32, byte: u8) -> u32 {
+    fn append(&self, state: u32, _byte: u8) -> u32 {
         state + 1
     }
 
@@ -72,6 +73,23 @@ impl Runner {
             curr_step: 0,
             trie: Rc::new(Box::new(TokTrie::from_env())),
             rec: Box::new(StackRecognizer::from(AnythingGoes {})),
+            tok_limit: 0,
+        }
+    }
+
+    fn stop(&mut self) {
+        let rec = StackRecognizer::from(FixedArgs {
+            text: "".to_string(),
+        });
+        self.rec = Box::new(rec);
+    }
+
+    fn next_step(&mut self) {
+        self.curr_step += 1;
+        if self.curr_step < self.program.steps.len() {
+            self.new_step();
+        } else {
+            self.stop();
         }
     }
 
@@ -80,6 +98,7 @@ impl Runner {
             Step::Fixed { text } => {
                 let rec = StackRecognizer::from(FixedArgs { text: text.clone() });
                 self.rec = Box::new(rec);
+                self.tok_limit = 0; // no limit
             }
             Step::Gen { max_tokens, rx } => {
                 if let Some(rx) = rx {
@@ -88,13 +107,13 @@ impl Runner {
                 } else {
                     self.rec = Box::new(StackRecognizer::from(AnythingGoes {}));
                 }
+                self.tok_limit = *max_tokens;
             }
         }
     }
 
     fn compute(&mut self) {
         // wprintln!("compute");
-        self.new_step();
         self.trie
             .compute_bias(&mut *self.rec, &mut self.helper.logit_biases);
     }
@@ -108,12 +127,17 @@ impl AiciVm for Runner {
     fn aici_process_prompt(&mut self) {
         wprintln!("prompt, {} tokens", self.helper.prompt_length);
         // ignore the prompt (for now)
+        self.new_step();
         self.compute();
     }
 
     fn aici_append_token(&mut self, token: u32) {
         let bytes = self.trie.token(token);
-        // wprintln!("xapp {} {:?}", token, bytes);
+        wprintln!("xapp {} {:?}", token, bytes);
+
+        // save the token, just in case
+        let toks = &mut self.helper.tokens;
+        toks.push(token);
 
         let rec = &mut *self.rec;
         for b in bytes {
@@ -121,9 +145,20 @@ impl AiciVm for Runner {
         }
         rec.collapse();
 
-        // save the token, just in case
-        let toks = &mut self.helper.tokens;
-        toks.push(token);
+        if self.tok_limit == 1 {
+            self.next_step();
+        } else if (0..=255).all(|byte| !rec.byte_allowed(byte)) {
+            if rec.special_allowed(SpecialToken::EndOfSentence) {
+                self.next_step();
+            } else {
+                wprintln!("no more bytes allowed, but no end of sentence");
+                self.stop();
+            }
+        } else {
+            if self.tok_limit > 0 {
+                self.tok_limit -= 1;
+            }
+        }
 
         self.compute();
     }
