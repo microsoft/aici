b53308076523359caf9ffb34fb3bb3fa2f2561d5
Michal Moskal <michal@moskal.me>
2023-10-16 09:53:18 -0700

    run 'perf' only on Linux

--- aici_ast_runner/native.sh
+++ aici_ast_runner/native.sh
@@ -8,4 +8,8 @@ set -e
 mkdir -p src/tokenizers
 (cd ../regex_llm && cargo run --release -- -t $T --save ../aici_ast_runner/tokenizer.bin)
 cargo build --release
-perf stat ./target/release/aici_ast_runner
+if [ `uname` = Linux ] ; then
+  perf stat ./target/release/aici_ast_runner
+else
+  ./target/release/aici_ast_runner
+fi
