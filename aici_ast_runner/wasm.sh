f9a8113379ee4d6e3661f31e3c5c69d8ca46cc1a
Michal Moskal <michal@moskal.me>
2023-10-17 18:54:40 +0000

    build fix

--- aici_ast_runner/wasm.sh
+++ aici_ast_runner/wasm.sh
@@ -2,13 +2,13 @@
 
 set -x
 set -e
-(cd ../aicirt && cargo build --release)
+# (cd ../aicirt && cargo build --release)
 cargo build --release --target wasm32-unknown-unknown
 cp target/wasm32-unknown-unknown/release/aici_ast_runner.wasm target/opt.wasm
 # wasm-opt -Oz target/wasm32-unknown-unknown/release/aici_ast_runner.wasm -o target/opt.wasm
 # wasm-strip target/opt.wasm
 # curl -X POST -T "target/opt.wasm" "http://127.0.0.1:8080/v1/aici_modules"
-exit
+# exit
 
 p=`pwd`
 cd ../aicirt
