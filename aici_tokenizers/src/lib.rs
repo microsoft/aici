ae843a640b851d8e6dc259bbd3dba5005d1367b4
Michal Moskal <michal@moskal.me>
2023-10-24 17:07:07 +0000

    add codellama tokenizer

--- aici_tokenizers/src/lib.rs
+++ aici_tokenizers/src/lib.rs
@@ -19,6 +19,7 @@ pub struct Tokenizer {
     pub info: Option<TokenInfo>,
     info_bytes: &'static [u8],
     pub hf_bytes: &'static [u8],
+    pub add_tokens: u32,
 }
 
 macro_rules! tok {
@@ -29,6 +30,17 @@ macro_rules! tok {
             info_bytes: include_bytes!(concat!("tokenizers/", $name, ".json")),
             hf_bytes: include_bytes!(concat!("hf-tokenizers/", $name, ".json")),
             info: None,
+            add_tokens: 0,
+        }
+    };
+    ($username:literal, $name:literal, $desc:literal, $add:literal) => {
+        Tokenizer {
+            name: $username.into(),
+            description: $desc.into(),
+            info_bytes: include_bytes!(concat!("tokenizers/", $name, ".json")),
+            hf_bytes: include_bytes!(concat!("hf-tokenizers/", $name, ".json")),
+            info: None,
+            add_tokens: $add,
         }
     };
 }
@@ -37,6 +49,7 @@ pub fn tokenizers() -> Vec<Tokenizer> {
     vec![
         tok!("gpt4", "cl100k_base, used by GPT-4 and GPT-3.5"),
         tok!("llama", "used by Llama, CodeLlama, etc."),
+        tok!("codellama", "llama", "used by Llama, CodeLlama, etc.", 16),
         tok!("falcon", "used by Falcon 7b, 40b, etc."),
         tok!("mpt", "MPT"),
         tok!("phi", "Phi 1.5"),
@@ -70,23 +83,31 @@ fn from_hex(hex_str: &str) -> Result<Vec<u8>> {
 impl Tokenizer {
     pub fn load(&mut self) {
         if self.info.is_none() {
-            self.info = Some(serde_json::from_slice::<TokenInfo>(self.info_bytes).unwrap());
+            let mut info = serde_json::from_slice::<TokenInfo>(self.info_bytes).unwrap();
+            let max = vec![
+                info.binary.values().max(),
+                info.special.values().max(),
+                info.text.values().max(),
+            ]
+            .iter()
+            .filter_map(|x| *x)
+            .max()
+            .unwrap();
+            assert!(*max < 1_000_000);
+            info.vocab_size = Some(max + 1);
+            let vocab_size = info.vocab_size.unwrap();
+            for off in 0..self.add_tokens {
+                info.special
+                    .insert(format!("<extra_id_{}>", off), vocab_size + off);
+            }
+            info.vocab_size = Some(vocab_size + self.add_tokens);
+            self.info = Some(info);
         }
     }
     pub fn tokrx_info(&self) -> TokRxInfo {
         let info = self.info.as_ref().unwrap();
-        let max = vec![
-            info.binary.values().max(),
-            info.special.values().max(),
-            info.text.values().max(),
-        ]
-        .iter()
-        .filter_map(|x| *x)
-        .max()
-        .unwrap();
-        assert!(*max < 1_000_000);
         TokRxInfo {
-            vocab_size: max + 1,
+            vocab_size: info.vocab_size.unwrap(),
             tok_eos: info.eos_token,
         }
     }
