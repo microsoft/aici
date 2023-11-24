7115e2d961fd206f4ef37a8dd27e6ccf5869fdc1
Michal Moskal <michal@moskal.me>
2023-11-24 18:26:06 +0000

    move llama here

--- infer/src/lib.rs
+++ infer/src/lib.rs
@@ -1,14 +1,15 @@
+pub mod llama;
+
 use std::{collections::HashSet, fmt::Display, path::PathBuf};
 
 use anyhow::{Error as E, Result};
 use candle::{DType, Device};
 use candle_nn::VarBuilder;
-use candle_transformers::models::llama as model;
 use hf_hub::{
     api::sync::{Api, ApiRepo},
-    Repo, RepoType,
+    RepoType,
 };
-use model::{Llama, LlamaConfig};
+use llama::{Llama, LlamaConfig};
 use tokenizers::Tokenizer;
 
 pub struct LoaderArgs {
@@ -17,15 +18,15 @@ pub struct LoaderArgs {
     pub local_weights: Option<String>,
 }
 
-enum AnyRepo {
+enum Repo {
     Api(ApiRepo),
     Local(String),
 }
 
-impl AnyRepo {
-    fn from(args: &LoaderArgs) -> Result<AnyRepo> {
+impl Repo {
+    fn from(args: &LoaderArgs) -> Result<Repo> {
         match &args.local_weights {
-            Some(path) => Ok(AnyRepo::Local(path.to_owned())),
+            Some(path) => Ok(Repo::Local(path.to_owned())),
             None => {
                 let api = Api::new()?;
                 let model_id = args
@@ -33,16 +34,20 @@ impl AnyRepo {
                     .clone()
                     .unwrap_or_else(|| "NousResearch/Llama-2-7b-hf".to_string());
                 let revision = args.revision.clone().unwrap_or("main".to_string());
-                let api = api.repo(Repo::with_revision(model_id, RepoType::Model, revision));
-                Ok(AnyRepo::Api(api))
+                let api = api.repo(hf_hub::Repo::with_revision(
+                    model_id,
+                    RepoType::Model,
+                    revision,
+                ));
+                Ok(Repo::Api(api))
             }
         }
     }
 
     fn get(&self, filename: &str) -> Result<PathBuf> {
         match self {
-            AnyRepo::Api(api) => api.get(filename).map_err(E::msg),
-            AnyRepo::Local(path) => Ok((path.to_owned() + filename).into()),
+            Repo::Api(api) => api.get(filename).map_err(E::msg),
+            Repo::Local(path) => Ok((path.to_owned() + filename).into()),
         }
     }
 
@@ -51,11 +56,11 @@ impl AnyRepo {
     }
 }
 
-impl Display for AnyRepo {
+impl Display for Repo {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            AnyRepo::Api(api) => write!(f, "{}", api.url("")),
-            AnyRepo::Local(path) => write!(f, "{}", path),
+            Repo::Api(api) => write!(f, "{}", api.url("")),
+            Repo::Local(path) => write!(f, "{}", path),
         }
     }
 }
@@ -64,17 +69,17 @@ pub fn load_llama(args: LoaderArgs) -> Result<(Tokenizer, Llama, Device)> {
     let device = Device::new_cuda(0)?;
     let dtype = DType::BF16;
 
-    let api = AnyRepo::from(&args)?;
-    println!("loading the model weights from {}", api);
+    let repo = Repo::from(&args)?;
+    println!("loading the model weights from {}", repo);
 
-    let tokenizer_filename = api.get("tokenizer.json")?;
+    let tokenizer_filename = repo.get("tokenizer.json")?;
 
-    let config: LlamaConfig = serde_json::from_slice(&api.read("config.json")?)?;
+    let config: LlamaConfig = serde_json::from_slice(&repo.read("config.json")?)?;
     let use_flash_attn = true;
     let config = config.into_config(use_flash_attn);
 
     let st_index: serde_json::Value =
-        serde_json::from_slice(&api.read("model.safetensors.index.json")?)?;
+        serde_json::from_slice(&repo.read("model.safetensors.index.json")?)?;
 
     let entries = st_index["weight_map"]
         .as_object()
@@ -87,11 +92,11 @@ pub fn load_llama(args: LoaderArgs) -> Result<(Tokenizer, Llama, Device)> {
     filenames.sort();
     let filenames = filenames
         .iter()
-        .map(|f| api.get(f))
+        .map(|f| repo.get(f))
         .collect::<Result<Vec<_>>>()?;
 
     println!("building the model");
-    let cache = model::Cache::new(true, dtype, &config, &device)?;
+    let cache = llama::Cache::new(true, dtype, &config, &device)?;
 
     let vb = unsafe { VarBuilder::from_mmaped_safetensors(&filenames, dtype, &device)? };
 
