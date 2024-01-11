9db012b642dbb78e46be42c9c78fbbaaa7ca1980
Michal Moskal <michal@moskal.me>
2024-01-11 20:44:18 +0000

    remove old scripts

--- scripts/old/bench-l.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-PYTHONPATH=. python benchmarks/benchmark_latency.py --model NousResearch/Llama-2-13b-chat-hf --tokenizer hf-internal-testing/llama-tokenizer
-
-# Avg latency: 2.836 seconds
--- scripts/old/bench-t.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-PYTHONPATH=. python benchmarks/benchmark_throughput.py --dataset benchmarks/small.json --num-prompts 200 --model NousResearch/Llama-2-13b-chat-hf --tokenizer hf-internal-testing/llama-tokenizer
-# Throughput: 4.12 requests/s, 2110.03 tokens/s, 858.21 out tokens/s
