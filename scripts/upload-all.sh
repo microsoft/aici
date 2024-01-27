0cdd78b22b60e87a95e17882aac4ab14217c7b9e
Michal Moskal <michal@moskal.me>
2024-01-26 16:28:55 -0800

    script for multi-upload of controllers

---
+++ scripts/upload-all.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+export AICI="$(cd `dirname $0`; pwd)/aici.sh --all-prefixes"
+
+case "$1" in
+  *aici-controllers-wasm32-wasi-*.tar.?z)
+    mkdir -p tmp/aici-controllers
+    tar --strip-components=1 -xf "$1" -C tmp/aici-controllers
+    if test -f tmp/aici-controllers/tag.sh ; then
+        cd tmp/aici-controllers
+        ./tag.sh --latest
+        rm -rf tmp/aici-controllers
+    else
+        echo "No tag.sh found in tmp/aici-controllers"
+        exit 1
+    fi
+    ;;
+  *)
+    echo "Usage: $0 aici-controllers-wasm32-wasi-....tar.[xz|gz]"
+    exit 1
+    ;;
+esac
