c57d37264d7ff776b7605b1dec14c97e4348f5f4
Michal Moskal <michal@moskal.me>
2024-03-05 22:14:42 +0000

    fix tsc install

--- scripts/test-jsctrl.sh
+++ scripts/test-jsctrl.sh
@@ -4,7 +4,7 @@ set -x
 cd `dirname $0`
 HERE=`pwd`
 cd $HERE/../controllers/jsctrl
+tsc --version || npm install -g typescript
 tsc -p samples
 PYTHONPATH=$HERE/../py \
 python3 ../pyctrl/driver.py samples/dist/test.js
-
