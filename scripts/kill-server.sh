f9526513876c77f8058703c0505fa292711bff7e
Michal Moskal <michal@moskal.me>
2024-04-23 23:03:51 +0000

    fix regex

--- scripts/kill-server.sh
+++ scripts/kill-server.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-P=`ps -ax|grep 'aicir[t]\|rllm-serve[r]\|/serve[r]\.sh\|node.*/buil[t]/worker|python[3] -m vllm.entrypoints' | awk '{print $1}' | xargs echo`
+P=`ps -ax|grep 'aicir[t]\|rllm-serve[r]\|/serve[r]\.sh\|node.*/buil[t]/worker\|python[3] -m vllm.entrypoints' | awk '{print $1}' | xargs echo`
 
 if [ "X$P" != "X" ] ; then 
   echo "KILL $P"
