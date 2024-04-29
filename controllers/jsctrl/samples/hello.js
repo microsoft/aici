a5466a79a1ade519e4a61106d68e5de4dd132303
Michal Moskal <michal@moskal.me>
2024-04-29 19:59:31 +0000

    remove accidental code

--- controllers/jsctrl/samples/hello.js
+++ controllers/jsctrl/samples/hello.js
@@ -1,13 +1,6 @@
-function fib(n) {
-    if (n <= 1) return n
-    return fib(n - 1) + fib(n - 2)
-}
-
 async function main() {
     await $`Ultimate answer is to the life, universe and everything is `
     await gen({ regex: /\d\d/ })
-    console.log(`fib(10) = ${fib(30)}`)
-    await gen({ regex: / [A-Z].*/ })
 }
 
 start(main)
