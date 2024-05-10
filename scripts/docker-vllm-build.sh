37dda265fd95595effe484fcde250b80f6296281
Michal Moskal <michal@moskal.me>
2024-05-10 15:56:27 +0000

    build vllm also for H100

--- scripts/docker-vllm-build.sh
+++ scripts/docker-vllm-build.sh
@@ -7,4 +7,4 @@ docker build . \
     --tag vllm/vllm-openai \
     --build-arg max_jobs=16 \
     --build-arg nvcc_threads=16 \
-    --build-arg torch_cuda_arch_list="8.0"
+    --build-arg torch_cuda_arch_list="8.0 9.0+PTX"
