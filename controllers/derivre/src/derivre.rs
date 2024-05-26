c807f66ebd259670557cfc2d582db89b4b8b2af1
Michal Moskal <michal@moskal.me>
2024-05-26 23:23:07 +0000

    more tests

--- controllers/derivre/src/derivre.rs
+++ controllers/derivre/src/derivre.rs
@@ -1,9 +1,62 @@
 use derivre::RegexVec;
 
+fn check_is_match(rx: &mut RegexVec, s: &str, exp: bool) {
+    if rx.is_match(s) == exp {
+    } else {
+        panic!(
+            "error for: {:?}; expected {}",
+            s,
+            if exp { "match" } else { "no match" }
+        );
+    }
+}
+
+fn match_(rx: &mut RegexVec, s: &str) {
+    check_is_match(rx, s, true);
+}
+
+fn match_many(rx: &mut RegexVec, ss: &[&str]) {
+    for s in ss {
+        match_(rx, s);
+    }
+}
+
+fn no_match(rx: &mut RegexVec, s: &str) {
+    check_is_match(rx, s, false);
+}
+
+fn no_match_many(rx: &mut RegexVec, ss: &[&str]) {
+    for s in ss {
+        no_match(rx, s);
+    }
+}
 
 fn main() {
     let mut rx = RegexVec::new_single("a[bc](de|fg)").unwrap();
     println!("{:?}", rx);
-    assert!(rx.is_match("abde"));
+    no_match(&mut rx, "abd");
+    match_(&mut rx, "abde");
+    no_match(&mut rx, "abdea");
+    println!("{:?}", rx);
+
+    let mut rx = RegexVec::new_single("a[bc]*(de|fg)*x").unwrap();
+
+    no_match_many(&mut rx, &["", "a", "b", "axb"]);
+    match_many(&mut rx, &["ax", "abdex", "abcbcbcbcdex", "adefgdefgx"]);
+    println!("{:?}", rx);
+
+    let mut rx = RegexVec::new_single("(A|foo)*").unwrap();
+    match_many(
+        &mut rx,
+        &["", "A", "foo", "Afoo", "fooA", "foofoo", "AfooA", "Afoofoo"],
+    );
+
+    let mut rx = RegexVec::new_single("[abcquv][abdquv]").unwrap();
+    match_many(
+        &mut rx,
+        &["aa", "ab", "ba", "ca", "cd", "ad", "aq", "qa", "qd"],
+    );
+    no_match_many(&mut rx, &["cc", "dd", "ac", "ac", "bc"]);
+
     println!("{:?}", rx);
 }
