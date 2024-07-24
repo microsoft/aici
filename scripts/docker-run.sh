a70c97d2d5ea4ae9a97a569f4b20c19f734ffc67
Michal Moskal <michal@moskal.me>
2024-07-24 01:06:43 +0000

    fix script

--- scripts/docker-run.sh
+++ scripts/docker-run.sh
@@ -5,34 +5,39 @@ ADD_ARGS=
 VLLM_ARGS="--port $PORT"
 DOCKER_ARGS=""
 
+if test -z "$DOCKER_TARGET" ; then
+    DOCKER_TARGET=vllm-general
+fi
+
+
 case "$1" in
-	--orca)
-		shift
-		ADD_ARGS="--model microsoft/Orca-2-13b --revision refs/pr/22 --aici-tokenizer=orca"
-		;;
-	--folder)
-		shift
-		D=`cd $1; pwd`
-		DOCKER_ARGS="--mount type=bind,source=$D,target=/vllm-workspace/model"
-		ADD_ARGS="--model ./model --aici-tokenizer ./model/tokenizer.json --tokenizer ./model"
-		shift
-		;;
-	--shell)
-		shift
-		DOCKER_ARGS="--entrypoint /bin/bash -it"
-		VLLM_ARGS=""
-		;;
+    --orca)
+        shift
+        ADD_ARGS="--model microsoft/Orca-2-13b --revision refs/pr/22 --aici-tokenizer=orca"
+        ;;
+    --folder)
+        shift
+        D=`cd $1; pwd`
+        DOCKER_ARGS="--mount type=bind,source=$D,target=/vllm-workspace/model"
+        ADD_ARGS="--model ./model --aici-tokenizer ./model/tokenizer.json --tokenizer ./model"
+        shift
+        ;;
+    --shell)
+        shift
+        DOCKER_ARGS="--entrypoint /bin/bash -it"
+        VLLM_ARGS=""
+        ;;
 esac
 
 set -x
 docker run \
-		--privileged \
-		--gpus=all \
-		--shm-size=8g \
+        --privileged \
+        --gpus=all \
+        --shm-size=8g \
         --mount source=profile,target=/root,type=volume \
-		-p $PORT:$PORT \
-		$DOCKER_ARGS \
-    aici/vllm-openai \
-	$VLLM_ARGS \
-	$ADD_ARGS \
-	"$@"
+        -p $PORT:$PORT \
+        $DOCKER_ARGS \
+    $DOCKER_TARGET \
+    $VLLM_ARGS \
+    $ADD_ARGS \
+    "$@"
